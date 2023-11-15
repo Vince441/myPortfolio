02e1d86b04858f4dafa088d3bc7628ec5388718c
Vince441 <vincentlouvart82@gmail.com>
2023-11-15 22:11:53 +0100

    responsive home et navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,49 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
 
-function determinActive( { isActive } ){
+  const handleShowMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
-if(isActive === true){
-return "active"
-}
-else {
-    return ""
-}
+  const handleCloseMenu = () => {
+    setShowMenu(false);
+  };
 
-}
-
+  function determineActive({ isActive }) {
+    if (isActive === true) {
+      return "active";
+    } else {
+      return "";
+    }
+  }
 
   return (
     <>
-    <div className='Navbar'>
-      
-      <div className='reseaux'>
-      <a href='https://www.linkedin.com/'><span className='reseau-1'><img src="./Linkedin.png" alt="Linkedin" /></span></a>
-        <a href='https://www.linkedin.com/'><span className='reseau-2'><img src="./git-white.png" alt="GitHub" /></span></a>
-      </div>
+      <div className="Navbar">
+        <div className='reseaux'>
+          <a href='https://www.linkedin.com/'><span className='reseau-1'><img src="./Linkedin.png" alt="Linkedin" /></span></a>
+          <a href='https://www.linkedin.com/'><span className='reseau-2'><img src="./git-white.png" alt="GitHub" /></span></a>
+        </div>
 
-      <div className='container-menu'>
-        <NavLink to="/" className={determinActive}>ACCUEIL</NavLink>
-        <NavLink to="Présentation" className={determinActive}>PRESENTATION</NavLink>
-        <NavLink to="Projet" className={determinActive}>PROJET</NavLink>
-        <NavLink to="Contact" className={determinActive}>CONTACT</NavLink>
-      </div>
+        <div className='container-menu'>
+          <button className='hamburger' onClick={handleShowMenu}><img src="./hamburger.png"/></button>
+          <div className={showMenu ? "menu" : ""}>
+            <NavLink to="/" className={determineActive} onClick={handleCloseMenu}>ACCUEIL</NavLink>
+            <NavLink to="Présentation" className={determineActive} onClick={handleCloseMenu}>PRESENTATION</NavLink>
+            <NavLink to="Projet" className={determineActive} onClick={handleCloseMenu}>PROJET</NavLink>
+            <NavLink to="Contact" className={determineActive} onClick={handleCloseMenu}>CONTACT</NavLink>
+          </div>
+        </div>
 
-      <div className='audio'><p>My playlist</p>
-        <audio controls src=""></audio>
+        <div className='audio'>
+          <p>My playlist</p>
+          <audio controls src=""></audio>
+        </div>
       </div>
-  </div>
-  </>
+    </>
   );
-  };
+};
+
