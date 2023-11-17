3981523a4bc69531e3d9bbc01a2f253204985cfa
vincentlouvart82@gmail.com <Vinc441>
2023-11-17 09:13:07 +0100

    ajout presentation et logo competence

--- src/pages/Presentation.jsx
+++ src/pages/Presentation.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 export default function Presentation() {
   return (
     <div className='Container-Presentation-page'>
-      <div className='Container-presentation'><h1>PRESENTATION</h1></div>
+      <div className='Container-presentation'><h1>PRESENTATION</h1>
+    <h2>COMPETENCES</h2>
+    <div className='Logo-competence'>
+<img src="./logo-competence/html-5.png" alt=""/>
+<img src="./logo-competence/css-3.png" alt=""/>
+<img src="./logo-competence/js.png" alt=""/>
+<img src="./logo-competence/react.png" alt=""/>
+<img src="./logo-competence/adobe-illustrator.png" alt=""/>
+<img src="./logo-competence/adobe-photoshop.png" alt=""/>
+<img src="./logo-competence/adobe-indesign.png" alt=""/>
+<img src="./logo-competence/figma.png" alt=""/>
+<img src="./logo-competence/github-white.png" alt=""/>
+<img src="./logo-competence/logo-git.png" alt=""/>
+<img src="./logo-competence/vscode.png" alt=""/>
+    </div>
+      </div>
       </div>
   )
 }
